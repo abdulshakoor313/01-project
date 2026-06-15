7bba1e13f0dcd7cd383c01dcd839169b17858d9d
Abdul Shakoor <ibupoto@gmail.com>
2026-06-15 21:55:57 +0500

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import MyCard from './Components/myCard'
+import MyCard from './Components/MyCard'
 
 function App() {
   return (
-    <div className="flex justify-center items-center gap-6 bg-gray-100 min-h-screen">
+    <div className="flex justify-center items-center gap-6 bg-gray-100">
       <MyCard
       name="Abdul Shakoor"
       description="Frontend developer learning React"
-      image="https://via.placeholder.com/400x200"
+      image="https://plus.unsplash.com/premium_photo-1781045230686-3b199165e902?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzN3x8fGVufDB8fHx8fA%3D%3D"
+      />
+
+       <MyCard
+      name="Mehran"
+      description="Class: 02"
+      image="https://images.unsplash.com/photo-1780764895105-ea3037466236?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
       />
 
     </div>
