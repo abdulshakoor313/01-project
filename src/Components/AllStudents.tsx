269d2cf329573d19c2c927f3ac0b606321424ebb
Abdul Shakoor <ibupoto@gmail.com>
2026-06-17 02:08:52 +0500

    ...

--- src/Components/AllStudents.tsx
+++ src/Components/AllStudents.tsx
@@ -1,27 +1,27 @@
 import StudentCard from './StudentCard';
 
-const studentData=[
-   {
-     image: "www.images/image12",
-    name: "Anas",
-    age: 10,
-    clas:"Two",
-    section:"A",
-   }
+const studentData = [
+    {
+        image: "www.images/image12",
+        name: "Anas",
+        age: 10,
+        clas: "Two",
+        section: "A",
+    }
 ];
 
 
 
 
-const AllStudents = ()=>(
-<div>
-{studentData.map((item,index)=>(
-    <StudentCard key={index}{...item}/>
-))}
+const AllStudents = () => (
+    <div>
+        {studentData.map((item, index) => (
+            <StudentCard key={index}{...item} />
+        ))}
 
 
 
-</div>
+    </div>
 );
 
 export default AllStudents;
--- src/Components/StudentCard.tsx
+++ src/Components/StudentCard.tsx
@@ -1,22 +1,22 @@
-type StudentDataTypes={
+type StudentDataTypes = {
     image: string;
-    name:string;
-    age:number;
-    clas:string;
-    section:string;
+    name: string;
+    age: number;
+    clas: string;
+    section: string;
 }
 
 
-function StudentCard({image,name,age,clas,section}:StudentDataTypes) {
-  return (
-    <div>
-      <img src={image}/>
-      <h1>Name{name}</h1>
-      <h2>Age{age}</h2>
-      <h3>Class{clas}</h3>
-      <h3>Section{section}</h3>
-    </div>
-  )
+function StudentCard({ image, name, age, clas, section }: StudentDataTypes) {
+    return (
+        <div>
+            <img src={image} />
+            <h1>Name{name}</h1>
+            <h2>Age{age}</h2>
+            <h3>Class{clas}</h3>
+            <h3>Section{section}</h3>
+        </div>
+    )
 }
 
 export default StudentCard
