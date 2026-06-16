89deca009c116a1e6c0dcb6f8321c7053fbc418a
Abdul Shakoor <ibupoto@gmail.com>
2026-06-17 02:20:14 +0500

    ....

--- src/Components/AllStudents.tsx
+++ src/Components/AllStudents.tsx
@@ -2,7 +2,7 @@ import StudentCard from './StudentCard';
 
 const studentData = [
     {
-        image: "www.images/image12",
+        image: "https://images.unsplash.com/photo-1777975433721-37a919288c7a?q=80&w=1632&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         name: "Anas",
         age: 10,
         clas: "Two",
