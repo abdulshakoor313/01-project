b38acc9a77353adc6c2a4de64de4a0247003f465
Abdul Shakoor <ibupoto@gmail.com>
2026-06-20 09:26:48 +0500

    ...

---
+++ src/Components/AllMembers.tsx
@@ -0,0 +1,19 @@
+import TeamMemberCard from "./TeamMemberCard"
+
+const AllMember=[
+    image: "URL";
+    name: "Abdul Shakoor";
+    designation:"CEO.";
+    department: ".......";
+
+]
+
+const AllMembers = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AllMembers
--- src/Components/TeamMemberCard.tsx
+++ src/Components/TeamMemberCard.tsx
@@ -1,3 +1,21 @@
-type TeanMeber={
-  
+import React from 'react'
+
+type TeamMember={
+  image: string;
+  name : string;
+  designation: string;
+  department: string;
+  email: string;
 }
+
+const TeamMemberCard = ((image,name,designation,department,email)) => {
+  return (
+    <div>
+      <img srce={image}/>
+      <h1>{name}</h1>
+      <h2>{designation}</h2>
+    </div>
+  )
+}
+
+export default TeamMemberCard
