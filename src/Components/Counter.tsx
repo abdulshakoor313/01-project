f35506d08982c0d86aeca1c28bae5fbd0b4346a7
Abdul Shakoor <ibupoto@gmail.com>
2026-06-21 11:08:42 +0500

    ....

---
+++ src/Components/Counter.tsx
@@ -0,0 +1,24 @@
+
+
+const Counter = () => {
+  return (
+<div className="max-w-sm rounded-xl overflow-hidden shadow-lg bg-white p-6 border-2 justify-around">
+      <img
+        className="w-full h-48 object-cover rounded-lg"
+        src="#"alt="Card"
+      />
+
+      <h2 className="text-2xl font-bold mt-4">Anas</h2>
+
+      <p className="text-gray-600 mt-2">Section A</p>
+      <h1>Class</h1>
+      <p>abc@gmail.com</p>
+
+      <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
+        Learn More
+      </button>
+    </div>
+  )
+}
+
+export default Counter
