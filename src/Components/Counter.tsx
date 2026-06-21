e965b03e1bc2fedf9ab7bd9e925d6959b111a67a
Abdul Shakoor <ibupoto@gmail.com>
2026-06-21 13:13:13 +0500

    ....

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -4,10 +4,10 @@ const Counter = () => {
   const  [count, setcount]=useState(0);
   return (
     <div>
-      <h1>{count}</h1>
-      <button  className="bg-red-500 text-white px-4 py-2 rounded" onClick={()=>setcount(count+1)}>Increase</button>
-      <button  className="bg-red-500 text-white px-4 py-2 rounded" onClick={()=>setcount(count-1)}>Decrease</button>
-      <button  className="bg-red-500 text-white px-4 py-2 rounded" onClick={()=>setcount(0)}>Reet</button>
+      <h1 className='text-4xl p-2 colo'>{count}</h1>
+      <button  className="bg-red-500 text-white px-4 py-2 rounded-4xl" onClick={()=>setcount(count+1)}>Increase</button>
+      <button  className="bg-red-500 text-white px-4 py-2 rounded-4xl" onClick={()=>setcount(count-1)}>Decrease</button>
+      <button  className="bg-red-500 text-white px-4 py-2 rounded-4xl" onClick={()=>setcount(0)}>Reet</button>
     </div>
   )
 }
