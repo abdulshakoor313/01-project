43547fd07243831990b241ad36182bcf5af07b72
Abdul Shakoor <ibupoto@gmail.com>
2026-06-21 13:07:48 +0500

    ...

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import  { useState } from 'react'
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
+  const  [count, setcount]=useState(0);
   return (
     <div>
       <h1>{count}</h1>
-       <button onClick={() => setCount(count + 1)}>+</button>
-      <button onClick={() => setCount(count - 1)}>-</button>
-      <button onClick={() => setCount(0)}>Reset</button>
+      <button onClick={()=>setcount(count+1)}>   Increase</button>
+      <button onClick={()=>setcount(count-1)}>   Decrease</button>
+      <button onClick={()=>setcount(0)}>   Reet</button>
     </div>
   )
 }
