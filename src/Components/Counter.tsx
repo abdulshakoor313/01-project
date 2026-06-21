bdc6c7a36948cdd3aed6f774cc6a340e949ede58
Abdul Shakoor <ibupoto@gmail.com>
2026-06-21 12:55:04 +0500

    .... used use state hook

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,31 +1,17 @@
-type Counter={
-    name: string;
-    description: string;
-    image: string;
-    designation:string;
-    email:string;
-    
-}
+import { useState } from 'react'
 
-const Counter = ({name,description,image,designation,email}:Counter) => {
+const Counter = () => {
+  const [count, setCount] = useState(0);
   return (
-<div className="max-w-sm rounded-xl overflow-hidden shadow-lg bg p-6 border-2 justify-around">
-      <img
-        className="w-full h-48 object-cover rounded-lg"
-        src={image}alt= "Card"
-      />
-
-      <h2 className="text-2xl font-bold mt-4">{name}</h2>
-
-      <p className="text-gray-600 mt-2">{description}</p>
-      <h1>{designation}</h1>
-      <p>{email}</p>
-
-      <button className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
-        Learn More
-      </button>
+    <div>
+      <h1>{count}</h1>
+       <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(0)}>Reset</button>
     </div>
   )
 }
 
 export default Counter
+
+
