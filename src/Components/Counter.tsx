2b0edce57220859b51cbc398f13b2a525c4b83ce
Abdul Shakoor <ibupoto@gmail.com>
2026-06-22 10:26:11 +0500

    ..

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -1,17 +1,15 @@
-import  { useState } from 'react'
+import { useState } from "react"
+
 
 const Counter = () => {
-  const  [count, setcount]=useState(0);
+  const [count,setcount]=useState(0);
   return (
     <div>
-      <h1 className='text-4xl p-2 colo'>{count}</h1>
-      <button  className="bg-red-500 text-white px-4 py-2 rounded-4xl" onClick={()=>setcount(count+1)}>Increase</button>
-      <button  className="bg-red-500 text-white px-4 py-2 rounded-4xl" onClick={()=>setcount(count-1)}>Increase</button>
-      <button  className="bg-red-500 text-white px-4 py-2 rounded-4xl" onClick={()=>setcount(0)}>Reet</button>
+      <h1>{count}</h1>
+
+      <button></button>
     </div>
   )
 }
 
 export default Counter
-
-
