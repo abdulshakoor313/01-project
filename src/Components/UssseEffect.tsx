c330598d16becf1a1cd71d25664d27d64afa0e74
Abdul Shakoor <ibupoto@gmail.com>
2026-06-23 01:06:15 +0500

    ...

--- src/Components/UssseEffect.tsx
+++ src/Components/UssseEffect.tsx
@@ -8,9 +8,9 @@ const UssseEffect = () => {
   return (
     <div>
       <h1>{Count}</h1>
-      <button onClick={()=>setCount(Count+1)}></button>
-      <button onClick={()=>setCount(Count-1)}></button>
-      <button onClick={()=>setCount(0)}></button>
+      <button onClick={()=>setCount(Count+1)}>Increase</button>
+      <button onClick={()=>setCount(Count-1)}>Decrease</button>
+      <button onClick={()=>setCount(0)}>Reset</button>
     </div>
   )
 }
