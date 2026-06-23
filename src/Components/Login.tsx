b73baf23eb5ddeba50d98343957a6ea87d4f99fb
Abdul Shakoor <ibupoto@gmail.com>
2026-06-23 12:43:12 +0500

    .....

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,29 +1,31 @@
-import { useState, useEffect } from "react"
+
 
 const Login = () => {
-    const [email, setemail] = useState("");
-    const [passWrod, setpassWord] = useState("");
-    useEffect(() => {
-        console.log("Signd In successfully");
-    }, [email, passWrod]);
-
-
-    return (
-        <div>
-            <form>
+    const submithandler=(e)=>{ 
+        console.log("Form submitted")
+        e.preventDefault()
+    }
+  return (
+    <div>
+        <form onClick={(e)=>submithandler(e)}>
                 <input 
                 className="border border-black-300"
                 type="text"
-                placeholder="Inter Email"
+                placeholder="Enter Email"
                 />
                 <input 
                  className="border border-black-300"
                 type="password"
-                placeholder="Inter password"
+                placeholder="Enter password"
                 />
+                <button
+                className="m-5 p-1 bg-gray-300 border border-black rounded"
+                type="submit">
+                Submit
+                </button>
             </form>
-        </div>
-    )
+    </div>
+  )
 }
 
 export default Login
