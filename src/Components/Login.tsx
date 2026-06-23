88a67c12ee86b7c5279230f159ac0af64490c676
Abdul Shakoor <ibupoto@gmail.com>
2026-06-23 11:13:06 +0500

    form handling.....

---
+++ src/Components/Login.tsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from "react"
+
+const Login = () => {
+    const [email, setemail]=useState("");
+    const [passWrod, setpassWord]=useState("");
+    useEffect(()=>{
+        console.log("Signd In successfully");
+    }, [email, passWrod]);
+
+
+  return (
+    <div>
+      <form/>
+    </div>
+  )
+}
+
+export default Login
