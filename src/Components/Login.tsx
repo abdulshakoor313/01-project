4beb6a1f5d266824218b412872e3f2b9660f47a4
Abdul Shakoor <ibupoto@gmail.com>
2026-06-24 13:53:54 +0500

    ....

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -2,42 +2,39 @@ import { useState } from "react"
 
 
 const Login = () => {
-const [email, setEmail]= useState("");
-const [passWord, setpassWord]= useState("");
-const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
-  e.preventDefault();
-};
-
-
-
+  const [email, setEmail] = useState("");
+  const [passWord, setpassWord] = useState("");
+  const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
 
   return (
     <div>
-      <form>
+      <form onSubmit={submitHandler}>
         <h1>Log In</h1>
-        <input 
-        className="border border-black"
-        type="email"
-        placeholder="Enter Email"
-        value={email}
-        onChange={(e)=>setEmail(e.target.value)}
+        <input
+          className="border border-black"
+          type="email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
         <h2>{email}</h2>
 
-        <input 
-        className="border border-black"
-        type="password"
-        placeholder="Enter password"
-        value={passWord}
-        onChange={(e)=>setpassWord(e.target.value)}
+        <input
+          className="border border-black"
+          type="password"
+          placeholder="Enter password"
+          value={passWord}
+          onChange={(e) => setpassWord(e.target.value)}
         />
         <h2>{passWord}</h2>
         <button
-        className="bg-black text-white p-2 mt-5 rounded-2xl"
-        type="submit"
-        onSubmit={submitHandler(e)=>}
-        >Submit</button>
-
+          className="bg-black text-white p-2 mt-5 rounded-2xl"
+          type="submit"
+        >
+          Submit
+        </button>
       </form>
     </div>
   )
