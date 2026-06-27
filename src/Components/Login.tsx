e7aaa0488d6ea118cc6d75c612a53f122baebf2b
Abdul Shakoor <ibupoto@gmail.com>
2026-06-27 11:00:17 +0500

    ......

---
+++ src/Components/Login.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react"
+
+const Login = () => {
+  const [Email, setEmail] = useState;
+  const [passWord, setpassWord] = useState;
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Login
