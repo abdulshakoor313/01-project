41d6d3190a7d296c98a8fb4f75432c157d42d610
Abdul Shakoor <ibupoto@gmail.com>
2026-06-27 11:38:51 +0500

    ......

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -3,39 +3,39 @@ import { useState } from "react"
 const Login = () => {
   const [Email, setEmail] = useState("");
   const [passWord, setpassWord] = useState("");
-  const submiithandler = (e:React.FormEvent<HTMLFormElement>)=>{
+  const submiithandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
   }
 
 
   return (
     <form onChange={submiithandler}>
-          <div>
-      <h1>Log In here</h1>
-      <input
-      className="w-full border border-gray-300 p-3 rounded-lg text-sm sm:text-base focus:outline-none focus:ring-2 focus:ring-sky-400"
-      type="text"
-      placeholder="Entr Email"
-      value={Email}
-      onChange={(e) => setEmail(e.target.value)}
-     
-      
-      />
+      <div>
+        <h1>Log In here</h1>
+        <input
+          className="w-full border border-gray-300 p-3 rounded-lg text-sm sm:text-base focus:outline-none focus:ring-2 focus:ring-sky-400"
+          type="text"
+          placeholder="Entr Email"
+          value={Email}
+          onChange={(e) => setEmail(e.target.value)}
 
-      <input
-      className="w-full border border-gray-300 p-3 rounded-lg text-sm sm:text-base focus:outline-none focus:ring-2 focus:ring-sky-400"
-      type="password"
-      placeholder="Password"
-      value={passWord}
-      onChange={(e) => setpassWord(e.target.value)}
-      />
 
-    </div>
-    <button
-    className="w-full bg-sky-500 text-white py-3 rounded-lg font-medium hover:bg-sky-600 active:scale-[0.98] transition"
-          type="submit"
-    >Submit
-    </button>
+        />
+
+        <input
+          className="w-full border border-gray-300 p-3 rounded-lg text-sm sm:text-base focus:outline-none focus:ring-2 focus:ring-sky-400"
+          type="password"
+          placeholder="Password"
+          value={passWord}
+          onChange={(e) => setpassWord(e.target.value)}
+        />
+
+      </div>
+      <button
+        className="w-full bg-sky-500 text-white py-3 rounded-lg font-medium hover:bg-sky-600 active:scale-[0.98] transition"
+        type="submit"
+      >Submit
+      </button>
     </form>
   )
 }
