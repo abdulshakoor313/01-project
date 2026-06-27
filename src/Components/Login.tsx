c2a805e98544859ed4e221ee3ed2dbb30ed1aa93
Abdul Shakoor <ibupoto@gmail.com>
2026-06-27 11:07:45 +0500

    ....

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -7,6 +7,7 @@ const Login = () => {
   return (
     <div>
       <input
+      className="w-full border border-gray-300 p-3 rounded-lg text-sm sm:text-base focus:outline-none focus:ring-2 focus:ring-sky-400"
       type="text"
       placeholder="Entr Email"
       value={Email}
@@ -15,7 +16,7 @@ const Login = () => {
       />
 
       <input
-      
+
       type="text"
       placeholder="Password"
       value={passWord}
