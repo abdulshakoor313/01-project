3ff3c9ac8a16591ce70047e0d03398561f6da849
Abdul Shakoor <ibupoto@gmail.com>
2026-06-30 12:57:32 +0500

    ...

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -3,7 +3,14 @@
 const NavBar = () => {
   return (
     <div>
-    
+        <div    >
+            <ul>
+                <li>Home</li>
+                <li>Products</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
     </div>
   )
 }
