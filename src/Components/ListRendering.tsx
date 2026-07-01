409af5f309b62d5c218104917c704413bc9b8165
Abdul Shakoor <ibupoto@gmail.com>
2026-07-01 11:09:37 +0500

    ......

---
+++ src/Components/ListRendering.tsx
@@ -0,0 +1,15 @@
+import { use, useState } from "react"
+const ListRendering = () => {
+    const [users] = useState([
+        { id:1, name:"Anas", age:10},
+        { id:1, name:"Onais", age:15}
+    ])
+    
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ListRendering
