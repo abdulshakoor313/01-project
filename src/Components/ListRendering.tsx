79fe79196be01ba1e755702268da6919319f1f15
Abdul Shakoor <ibupoto@gmail.com>
2026-07-01 11:25:36 +0500

    ......

--- src/Components/ListRendering.tsx
+++ src/Components/ListRendering.tsx
@@ -2,11 +2,18 @@ import {useState} from 'react'
 
 const ListRendering = () => {
     const [users] = useState([
-        {id: 1, name:"Anas", }
-    ])
+        {id: 1, name:"Anas", age:10},
+        {id: 1, name:"Onais", age:15},
+    ]);
   return (
     <div>
-      
+      <h1>Usr List</h1>
+      {users.map((user)=>(
+        <div key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.age}</p>
+        </div>
+      ))}
     </div>
   )
 }
