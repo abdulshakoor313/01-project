182acd8906fada7b336e9df3a32c1ef22f9be34f
Abdul Shakoor <ibupoto@gmail.com>
2026-07-01 11:21:37 +0500

    ......

--- src/Components/ListRendering.tsx
+++ src/Components/ListRendering.tsx
@@ -1,10 +1,9 @@
-import { use, useState } from "react"
+import {useState} from 'react'
+
 const ListRendering = () => {
     const [users] = useState([
-        { id:1, name:"Anas", age:10},
-        { id:1, name:"Onais", age:15}
+        {id: 1, name:"Anas", }
     ])
-    
   return (
     <div>
       
